f83de32c2f2c2ace7a9ea0ca1b2894de4c66b7f8
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-28 16:47:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,11 +104,12 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
+         programmer_hash[:alan_kay][:known_for] = "GUI"
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      #alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     puts programmer_hash
+
      
      
 end
